1bfafe97ca0a81ae5e8503e39456f3fbffaf0b09
Dominik Rohal <rohal.dominik.123@gmail.com>
2022-09-06 14:28:47 +0300

    exercise 1.1 code finalised and reformat

--- courseinfo/src/App.js
+++ courseinfo/src/App.js
@@ -20,32 +20,34 @@ const Content = (props) => {
     )
 }
 
-const Total = () => {
-    
+const Total = (props) => {
+    return (
+        <p>Number of exercises {props.total}</p>
+    )
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
-    
+    const course = 'Half Stack application development'
+    const part1 = 'Fundamentals of React'
+    const exercises1 = 10
+    const part2 = 'Using props to pass data'
+    const exercises2 = 7
+    const part3 = 'State of a component'
+    const exercises3 = 14
+
     let parts = [
         {name: part1, exercises: exercises1},
         {name: part2, exercises: exercises2},
         {name: part3, exercises: exercises3},
     ]
-  return (
-      <div>
-        <Header course={course}/>
-          <Content parts = {parts}/>
-        
-        <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
-      </div>
-  )
+
+    return (
+        <div>
+            <Header course={course}/>
+            <Content parts={parts}/>
+            <Total total = {exercises1 + exercises2 + exercises3}/>
+        </div>
+    )
 }
 
 export default App
